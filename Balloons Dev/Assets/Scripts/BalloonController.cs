610ebc86798c543d620bb9d017f81b47e807f8cb
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2026-01-29 16:50:49 -0700

    make balloon a prefab, make a balloon controller object with a BalloonController.cs script attached to it

--- Balloons Dev/Assets/Scripts/BalloonController.cs
+++ Balloons Dev/Assets/Scripts/BalloonController.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
 
-public class DestroyParticle : MonoBehaviour
+public class BalloonController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Destroy(gameObject, 1f);
+        
     }
 
     // Update is called once per frame
