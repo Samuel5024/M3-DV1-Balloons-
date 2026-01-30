f0b4648f5ca79be431ef9c7f9fe628dd2453898b
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2026-01-29 17:40:35 -0700

    update BalloonController.cs

--- Balloons Dev/Assets/Scripts/BalloonController.cs
+++ Balloons Dev/Assets/Scripts/BalloonController.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class BalloonController : MonoBehaviour
 {
+    public GameObject balloonPrefab;
+    public float floatStrength = 20f;
+     private GameObject balloon;
    void Update()
     {
         if(Input.GetButtonDown("XRI_Right_TriggerButton"))
         {
             Debug.Log("Trigger down");
+            CreateBalloon();
         }
-        else if(Input.GetButtonUp("XRI_Right_TriggerButton"))
+        // else if(Input.GetButtonUp("XRI_Right_TriggerButton"))
+        if(Input.GetButtonUp("XRI_Right_TriggerButton"))
         {
             Debug.Log("Trigger up");
+            ReleaseBalloon();
         }
+    }
+
+    public void CreateBalloon()
+    {
+        balloon = Instantiate(balloonPrefab);
+
+    }
+
+    public void ReleaseBalloon()
+    {
+        Debug.Log("Release called");
+        Rigidbody rb = balloon.GetComponent<Rigidbody>();
+        Vector3 force = Vector3.up * floatStrength;
+        rb.AddForce(force);
+
+        GameObject.Destroy(balloon, 10f);
+        balloon = null;
     }
 }
